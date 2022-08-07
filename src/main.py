9bb2d810f6dd9df84f711f089bbbf5af30da83b2
aemem <anamoliner.marques@gmail.com>
2022-08-07 15:57:50 +0000

    primeras correcciones

--- src/main.py
+++ src/main.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Characters, Planets, Vehicles, Favorites
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -34,51 +33,56 @@ def sitemap():
 @app.route('/users', methods=['GET'])
 def get_users():
    users = User.query.all()
-   return jsonify({"users": users}), 200
+   users_serialized = list(map(lambda item: item.serialize(), users))
+   return jsonify({"users": users_serialized}), 200
 
 @app.route('/users/favorites', methods=['GET'])
 def get_user_favs():
     favs = Favorites.query.all()
-    return jsonify({"favorites": favorites})
+    favs_serialized = list(map(lambda item: item.serialize(), favs))
+    return jsonify({"favorites": favs_serialized}), 200
 
 #character routes
 @app.route('/characters', methods=['GET'])
 def get_characters():
     characters = Characters.query.all()
-    return jsonify({"characters": characters}), 200
+    characters_serialized = list(map(lambda item: item.serialize(), characters))
+    return jsonify({"characters": characters_serialized}), 200
    
-@app.route('/character/<int: char_id>', methods=['GET'])
-def get_character_by_id(char_id):
-    char = Characters.query.get(char_id)
-    return jsonify({"character": char.serialize()}), 200
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character_by_id(character_id):
+    char = Characters.query.get(character_id)
+    return jsonify({"character": char}), 200
 
 #planet routes
 @app.route('/planets', methods=['GET'])
 def get_planets():
     planets = Planets.query.all()
-    return jsonify({"planets": planets}), 200
+    planets_serialized = list(map(lambda item: item.serialize(), planets))
+    return jsonify({"planets": planets_serialized}), 200
  
-@app.route('/planet/<int: character_id>', methods=['GET'])
-def get_planet_by_id(plan_id):
-    plan = Planets.query.get(plan_id)
-    return jsonify({"planet": plan.serialize()}), 200
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_planet_by_id(planet_id):
+    plan = Planets.query.get(planet_id)
+    return jsonify({"planet": plan}), 200
 
 #vehicle routes
 @app.route('/vehicles', methods=['GET'])
 def get_vehicles():
     vehicles = Vehicles.query.all()
-    return jsonify({"vehicles": vehicles}), 200
+    vehicles_serialized = list(map(lambda item: item.serialize(), vehicles))
+    return jsonify({"vehicles": vehicles_serialized}), 200
 
-@app.route('/vehicle/<int: chavehicle_id>', methods=['GET'])
-def get_vehicle_by_id(vehi_id):
-    vehi = Vehicles.query.get(vehi_id)
-    return jsonify({"vehicle": vehi.serialize()}), 200
+@app.route('/vehicle/<int:vehicle_id>', methods=['GET'])
+def get_vehicle_by_id(vehicle_id):
+    vehi = Vehicles.query.get(vehicle_id)
+    return jsonify({"vehicle": vehi}), 200
 
 #add favs
 @app.route('/favorite/character/<int:character_id', methods=['POST'])
 def add_fav_character():
-    body_characters_id = request.json.get("characters_id")
-    favorite_characters = Favorite_characters(characters_id=body_characters_id)
+    body_characters_id = request.json.get("character_id")
+    favorite_characters = Favorite_characters(characters_id=body_character_id)
     db.session.add(favorite_characters)
     db.session.commit()
     return jsonify({"favortie characters": favorites.characters_id}), 200
@@ -89,7 +93,7 @@ def add_fav_planet():
     favorite_planets = Favorite_planets(planets_id=body_planets_id)
     db.session.add(favorite_planets)
     db.session.commit()
-    return jsonify({"favortie planets": favorites.planets_id}, 200
+    return jsonify({"favortie planets": favorites.planets_id}), 200
 
 @app.route('/favorite/vehicle/<int:vehicle_id', methods=['POST'])
 def add_fav_vehicle():
@@ -97,7 +101,7 @@ def add_fav_vehicle():
     favorite_vehicles = Favorite_cvehicles(vehicles_id=body_vehicles_id)
     db.session.add(favorite_vehicles)
     db.session.commit()
-    return jsonify({"favortie vehicles": favorites.vehicles_id}, 200
+    return jsonify({"favortie vehicles": favorites.vehicles_id}), 200
 
 #delete favs
 @app.route('/favorite/character/<int:character_id', methods=['DELETE'])
